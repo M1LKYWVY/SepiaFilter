0ce299e77183fc3840512a849cb394febc57dcd4
= <=>
2017-12-27 16:57:43 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/SPLLAB7.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SPLLAB7.dir/main.c.o"
+  "SPLLAB7.pdb"
+  "SPLLAB7.exe"
+  "libSPLLAB7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SPLLAB7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SPLLAB7.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SPLLAB7.
+# This may be replaced when dependencies are built.
